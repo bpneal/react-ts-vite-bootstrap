d5b35adfcd976145f218cd99283f998804e1e29d
Ben Neal <ben.neal@origamienergy.com>
2025-02-26 11:42:37 +0000

    Add Prettier, Husky and Lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  {
+    settings: { react: { version: 'detect' } },
+    plugins: { react },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/display-name': 'off',
+      'react/prop-types': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+    },
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -25,4 +39,5 @@ export default tseslint.config(
       ],
     },
   },
-)
+  eslintPluginPrettierRecommended,
+);
